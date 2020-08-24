99ae3b1df2b5c8b07a511d470d37957bb0e168cc
Irla Andrade <paula.irla@gmail.com>
2020-08-24 15:23:01 -0300

    implementação das páginas login e signup

--- todolistvuejs/src/pages/AtividadesAtribuidas.vue
+++ todolistvuejs/src/pages/AtividadesAtribuidas.vue
@@ -4,11 +4,16 @@
     <header>
       <h2>Atividades atribuídas a mim</h2>
     </header>
+    <section>
+      <h3>Oi</h3>
+    </section>
   </q-page>
 </template>
 
 <script>
 export default {
   // name: 'PageName',
+  methods: {
+  }
 }
 </script>
